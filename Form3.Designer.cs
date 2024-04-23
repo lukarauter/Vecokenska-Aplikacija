4b95a3faa5e9430385773dfde1b6de8aac019bfc
Luka <lukarauter19@gmail.com>
2024-04-23 17:12:49 +0200

    Done deal

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -29,8 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,6 +40,7 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(2, 3);
@@ -48,16 +50,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Izračun povrečne cene avtomobilov";
             // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(6, 42);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 32);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "OBDELAJ";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -66,6 +58,24 @@
             this.label1.Size = new System.Drawing.Size(372, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Z tem gumbom spodaj se izračuna povprečna cena vseh avtomobilov v tabeli";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(191, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OBDELAJ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 2;
             // 
             // Form3
             // 
@@ -87,5 +97,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace vecokenska_aplikacija
 {
@@ -23,5 +24,45 @@ namespace vecokenska_aplikacija
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Access Form1 to get data
+            Form1 form1 = Application.OpenForms.OfType<Form1>().FirstOrDefault(); // Find Form1 instance
+            if (form1 != null)
+            {
+                DataGridView dataGridView1 = form1.Controls.Find("dataGridView1", true).FirstOrDefault() as DataGridView;
+                if (dataGridView1 != null)
+                {
+                    double sum = 0;
+                    int count = 0;
+
+                    // Iterate over rows and calculate sum
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (row.Cells[3].Value != null && double.TryParse(row.Cells[3].Value.ToString(), out double value))
+                        {
+                            sum += value;
+                            count++;
+                        }
+                    }
+
+                    // Calculate average sum
+                    double average = count > 0 ? sum / count : 0;
+
+                    // Display the average sum in TextBox in Form3
+                    textBox1.Text = average.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("DataGridView1 not found in Form1.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Form1 not found.");
+            }
+        }
+
     }
 }
